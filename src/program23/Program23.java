8ad57c28dc8aa63f0122a49852989f5bfb4ade40
lucassantiago07 <slucassantiago@hotmail.com>
2019-04-11 21:55:14 -0300

    Mensagem de bom dia

--- src/program23/Program23.java
+++ src/program23/Program23.java
@@ -1,21 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package program23;
-
-/**
- *
- * @author User
- */
 public class Program23 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        
+        System.out.println("Bom dia !");
+        
+        
+        
+        
     }
     
 }
